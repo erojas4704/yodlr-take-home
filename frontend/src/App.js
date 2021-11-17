652890b4f73a8d2a54992894d5b2f8da309a641c
erojas4704 <55060308+erojas4704@users.noreply.github.com>
2021-11-17 16:46:27 -0500

    Added navbar to home page and integrated react bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,46 @@
-import './App.css';
-import { useEffect } from 'react';
-import axios from 'axios';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import Admin from './Admin';
-import SignUp from './SignUp';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Link, NavLink, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Admin from "./Admin";
+import SignUp from "./SignUp";
+import Container from "react-bootstrap/Container";
+import { Nav, Navbar } from "react-bootstrap";
 
 function App() {
   useEffect(() => {
-
     const getUsers = async () => {
-      const ef = await axios.get('/users');
+      const ef = await axios.get("/users");
       console.log(ef.data);
-    }
+    };
     getUsers();
-  }, [])
+  }, []);
 
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact element={<Home/>} />
-          <Route path="/admin" exact element={<Admin/>} />
-          <Route path="/signup" exact element={<SignUp/>} />
-        </Switch>
-      </Router>
-    </div>
+    <Router>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="me-auto">
+              ASS NUGGET
+              <Nav.Link> <NavLink to="home">Home</NavLink></Nav.Link>
+              <Nav.Link> <NavLink to="admin">Admin</NavLink></Nav.Link>
+              <Nav.Link> <NavLink to="signup">Sign Up</NavLink></Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/admin" exact element={<Admin />} />
+          <Route path="/signup" exact element={<SignUp />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,9 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
 export default function Home() {
     return (<>
-        <h1>Yodlr Design Challenge</h1>
         <p>
             <a href="/signup">Registration Page</a><br />
             <a href="/admin">Admin Page</a>
+            <Button>Do it</Button>
         </p>
     </>
     )
